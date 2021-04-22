27f84102fc9a40fc93e0e8423be753641c0d1827
Samurus <achenbachj@web.de>
2021-04-22 08:53:00 +0200

    heroku compatibility

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,57 +1,55 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm")
-	kotlin("plugin.spring") version "1.3.61"
-	id("io.spring.dependency-management")
-	id("org.springframework.boot") version "2.2.4.RELEASE"
+    kotlin("jvm")
+    kotlin("plugin.spring") version "1.3.61"
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") version "2.2.4.RELEASE"
 }
 
 val developmentOnly by configurations.creating
 configurations {
-	runtimeClasspath {
-		extendsFrom(developmentOnly)
-	}
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	// implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.2")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(module = "junit")
-	}
-	testImplementation("org.junit.jupiter:junit-jupiter-engine")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
-if (project.hasProperty("prod")) {
-	tasks.withType<Jar> {
-		dependsOn(":frontend:yarn_build")
+tasks.withType<Jar> {
+    dependsOn(":frontend:yarn_build")
 
-		from("../frontend/build") {
-			into("static")
-		}
-	}
+    from("../frontend/build") {
+        into("static")
+    }
 }
 
 tasks.register("stage") {
-	dependsOn("bootJar")
+    dependsOn("bootJar")
 }
 
 
